60496d8ce4f35fa51779cef7558fee28592163be
Alessandro Vizin <alessandro.vizin@valtech.de>
2021-06-11 20:26:20 +0200

    Add room restrictions where reservations are submitted

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,5 +3,6 @@ package repository
 import "github.com/Vizz85/go-bookings/internal/models"
 
 type DatabaseRepo interface {
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
 }
