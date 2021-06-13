764fb6a33a11474b82abaf5582cd81b5f2c35b94
Alessandro Vizin <alessandro.vizin@valtech.de>
2021-06-13 11:25:44 +0200

    Add Auth middleware and Login handlers

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 
+	"github.com/Vizz85/go-bookings/internal/helpers"
+
 	"github.com/justinas/nosurf"
 )
 
@@ -23,4 +25,15 @@ func NoSurf(next http.Handler) http.Handler {
 // SessionLoad loads and saves session data for current request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !helpers.IsAuthenticated(r) {
+			session.Put(r.Context(), "error", "Log in first")
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
